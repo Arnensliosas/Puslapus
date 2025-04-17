c13b41b29d4503b794a2169f322d85f891c74fa0
Arnensliosas <arnasnlc2007@gmail.com>
2025-04-17 22:52:02 +0300

    added error messages on empty login and registration inputs

--- static/loginas.js
+++ static/loginas.js
@@ -1,4 +1,16 @@
 document.getElementById("loginButton").addEventListener("click", function (event) {
     event.preventDefault(); // stops the form from actually submitting
+
+    const loginName = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const errorMessage = document.getElementById("error-message");
+
+    if (loginName === "" || password === "") {
+      errorMessage.textContent = "Nu susikaupk seni uzpildyk tas eilutes ir prisijunk";
+  } else {
+    errorMessage.textContent = "";
+    alert("tu gejus");
+  }
+
     console.log("Login button clicked!");
   });
--- static/registrationas.js
+++ static/registrationas.js
@@ -1,4 +1,16 @@
 document.getElementById("registerButton").addEventListener("click", function (event) {
     event.preventDefault(); // stops the form from actually submitting
+
+    const registerName = document.getElementById("reg-username").value.trim();
+    const password = document.getElementById("reg-password").value.trim();
+    const errorMessage = document.getElementById("error-message");
+
+    if (registerName === "" || password === "") {
+      errorMessage.textContent = "Nu susikaupk seni uzpildyk tas eilutes ir uzsiregistruok";
+  } else {
+    errorMessage.textContent = "";
+    alert("tu gejus");
+    
+  }
     console.log("register button clicked!");
   });
